94010813e6ac6c0aba88fb940484344da0e9a284
CIANDT\lurian <lurian.azar-ext@ab-inbev.com>
2023-01-04 16:42:03 -0300

    Create the product screen

---
+++ app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
@@ -0,0 +1,69 @@
+package br.com.alura.aluvery.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alura.aluvery.ui.theme.AluveryTheme
+
+class ProductFormActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AluveryTheme() {
+                Surface() {
+                    ProductFormScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductFormScreen() {
+    Column(
+        Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(text = "Criando o produto")
+        var url by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = url,
+            onValueChange = { url = it },
+            label = { Text(text = "Url da imagem") }
+        )
+
+        var name by remember { mutableStateOf("") }
+        TextField(value = name, onValueChange = { name = it })
+
+        var price by remember { mutableStateOf("") }
+        TextField(value = price, onValueChange = { price = it })
+
+        var description by remember { mutableStateOf("") }
+        TextField(value = description, onValueChange = { description = it })
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Salvar")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    AluveryTheme() {
+        Surface() {
+            ProductFormScreen()
+        }
+    }
+}
